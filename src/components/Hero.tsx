df9196a2b4392436d1db18d7279be944939896b8
protocol113 <protocol113@protonmail.com>
2024-11-07 17:18:28 -0500

    added public folder

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ export function Hero() {
       <div
         className="absolute inset-0 z-0"
         style={{
-          backgroundImage: "../../img/BGHero.jpeg",
+          backgroundImage: "/public/img/BGHero.jpeg",
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           filter: 'brightness(0.3)',
