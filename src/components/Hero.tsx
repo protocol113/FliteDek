4f20e9b5451cec5dff961a8ab602d9939e82d87e
protocol113 <protocol113@protonmail.com>
2024-11-07 16:59:26 -0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ export function Hero() {
       <div
         className="absolute inset-0 z-0"
         style={{
-          backgroundImage: 'url(/img/BGHero.jpeg)',
+          backgroundImage: src="/img/BGHero.jpeg",
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           filter: 'brightness(0.3)',
